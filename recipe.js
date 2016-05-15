a2a4022f4634ec6c76084e817a426b35e3a7c038
deniznida <deniz@flatironschool.com>
2016-05-15 00:34:28 -0400

    done

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(recipe, name, amounts) {
+  recipe[name] = amounts;
+  return recipe;
+}
+
+function removeIngredient(recipe, name) {
+  delete recipe[name];
+  return recipe;
+}
+
+function updateIngredient(recipe, name, amounts) {
+  recipe[name] = amounts;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for(var key in recipe){
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
